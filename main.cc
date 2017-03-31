68bfdc05f442ff377f3094eced5691b05868af39
Charlie Somerville <charlie@charlie.bz>
2017-03-31 18:15:47 +1100

    take source file from argv

--- main.cc
+++ main.cc
@@ -1,11 +1,21 @@
 #include <ruby_parser/parser.hh>
 #include <iostream>
+#include <fstream>
 
-int main()
+int main(int argc, const char** argv)
 {
-  ruby_parser::parser::typedruby24 p {
-    "a = 1 + 2"
-  };
+  if (argc < 2) {
+    std::cerr << "usage: " << argv[0] << " <source file>" << std::endl;
+    return 1;
+  }
+
+  std::ifstream file { argv[1], std::ios_base::in };
+
+  std::string source;
+
+  file >> source;
+
+  ruby_parser::parser::typedruby24 p { source };
 
   p.parse();
 }
