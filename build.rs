51971fd0760dded2f7345b88952c24a6c30eac4f
Charlie Somerville <charlie@charlie.bz>
2017-04-04 16:28:19 +1000

    fail build if make fails

--- build.rs
+++ build.rs
@@ -1,10 +1,13 @@
 use std::env;
+use std::process;
 use std::process::Command;
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
 
-    Command::new("make").status().unwrap();
+    if !Command::new("make").status().unwrap().success() {
+        process::exit(1);
+    }
 
     println!("cargo:rustc-link-search=native={}", env::current_dir().unwrap().display());
 }
