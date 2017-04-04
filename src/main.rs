b88df30cb727ca66c208f8533b9b46ff4dbeb3c2
Charlie Somerville <charlie@charlie.bz>
2017-04-04 16:30:45 +1000

    take filename to parse as argument

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,26 @@ mod ffi;
 mod ast;
 mod parser;
 
+use std::env;
+use std::process;
+use std::io::Write;
+use std::io::prelude::*;
+use std::fs::File;
+
 fn main() {
-    println!("{:#?}", parser::parse("foo.rb", "123 + 456"));
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        writeln!(&mut std::io::stderr(), "usage: {} <source file>", args[0]);
+        process::exit(1);
+    }
+
+    let filename = &args[1];
+
+    let mut source = String::new();
+
+    File::open(filename).unwrap()
+        .read_to_string(&mut source).unwrap();
+
+    println!("{:#?}", parser::parse(filename, source.as_str()));
 }
