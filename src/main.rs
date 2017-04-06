dc364141104f6da69b5b82b45a188af14eaeedb8
Hailey Somerville <hailey@hailey.lol>
2017-04-06 17:57:57 +1000

    take multiple filenames on argv

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,26 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
-        writeln!(&mut std::io::stderr(), "usage: {} <source file>", args[0]).unwrap();
+        writeln!(&mut std::io::stderr(), "usage: {} <source files...>", args[0]).unwrap();
         process::exit(1);
     }
 
-    let filename = &args[1];
+    let mut rc = 0;
 
-    let mut source = String::new();
+    for filename in args.iter().skip(1) {
+        let result = File::open(filename).and_then(|mut file| {
+            let mut source = String::new();
+            file.read_to_string(&mut source).map(|_| source)
+        });
 
-    File::open(filename).unwrap()
-        .read_to_string(&mut source).unwrap();
+        match result {
+            Ok(source) => { parser::parse(filename.as_str(), source.as_str()); }
+            Err(e) => {
+                rc = 1;
+                writeln!(&mut std::io::stderr(), "{}: {}", filename, e).unwrap();
+            }
+        };
+    }
 
-    println!("{:#?}", parser::parse(filename, source.as_str()));
+    process::exit(rc);
 }
