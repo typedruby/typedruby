cd0f43642448b25bb0b0c524f52c4eb4670cf3f0
Hailey Somerville <hailey@hailey.lol>
2017-04-10 14:38:22 +1000

    show error line number

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,57 @@ mod parser;
 
 use std::env;
 use std::process;
+use std::io;
 use std::io::Write;
 use std::io::prelude::*;
 use std::fs::File;
+
+struct SourceFile {
+    filename: String,
+    source: String,
+    line_map: Vec<usize>,
+}
+
+impl SourceFile {
+    fn open(filename: &str) -> io::Result<SourceFile> {
+        let mut file = File::open(filename)?;
+
+        let mut source = String::new();
+        file.read_to_string(&mut source)?;
+
+        let mut line_map = vec![];
+        for (index, c) in source.char_indices() {
+            if c == '\n' {
+                line_map.push(index);
+            }
+        }
+
+        Ok(SourceFile {
+            filename: filename.to_owned(),
+            source: source,
+            line_map: line_map,
+        })
+    }
+
+    fn parse(&self) -> ast::Ast {
+        parser::parse(self.filename.as_str(), self.source.as_str())
+    }
+
+    fn line_for_pos(&self, byte_pos: usize) -> usize {
+        match self.line_map.binary_search(&byte_pos) {
+            Ok(idx) => idx + 1,
+            Err(idx) => idx + 1,
+        }
+    }
+}
+
+fn show_diagnostic<'r>(diagnostic: &'r &ast::Diagnostic) -> bool {
+    match diagnostic.level {
+        ast::DiagnosticLevel::Error |
+        ast::DiagnosticLevel::Fatal => true,
+        _ => false
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -21,20 +69,16 @@ fn main() {
     for filename in args.iter().skip(1) {
         println!("\x1b[34;1m{}\x1b[0m", filename);
 
-        let result = File::open(filename).and_then(|mut file| {
-            let mut source = String::new();
-            file.read_to_string(&mut source).map(|_| source)
-        });
+        let file = SourceFile::open(filename).unwrap();
 
-        match result {
-            Ok(source) => {
-                parser::parse(filename.as_str(), source.as_str());
-            }
-            Err(e) => {
-                rc = 1;
-                println!("{:?}", e);
-            }
-        };
+        let result = file.parse();
+
+        for diagnostic in result.diagnostics.iter().filter(show_diagnostic) {
+            println!("\x1b[31;1m{}:\x1b[0m {:?}", file.line_for_pos(diagnostic.loc.begin_pos), diagnostic.message);
+            rc = 1;
+        }
+
+        // println!("{:#?}", result.ast);
     }
 
     process::exit(rc);
