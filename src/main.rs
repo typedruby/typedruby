68ef574a1675a91fcbf254b37a3ebd4194349ab0
Charlie Somerville <charlie@charlie.bz>
2017-04-20 17:50:50 +1000

    start hooking up error reporting

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 mod ast;
 mod environment;
+mod errors;
 mod object;
 mod top_level;
 
 use environment::Environment;
+use errors::ErrorReporter;
 
 use std::env;
+use std::io;
 
 fn main() {
-    let env = Environment::new();
+    let mut errors = ErrorReporter::new(io::stderr());
+    let env = Environment::new(&mut errors);
 
     for arg in env::args() {
         env.load_file(arg);
