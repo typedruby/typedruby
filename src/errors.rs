b5ffed3b35f646c4160a1ee00357f927698e21db
Charlie Somerville <charlie@charlie.bz>
2017-04-21 15:07:33 +1000

    support warnings too

--- src/errors.rs
+++ src/errors.rs
@@ -2,15 +2,21 @@ use std::io::Write;
 
 use ast::{Loc, SourceFile};
 
+type Details<'a> = [(&'a str, &'a SourceFile, &'a Loc)];
+
 pub trait ErrorSink {
-    fn error(&mut self, message: &str, details: &[(&str, &SourceFile, &Loc)]);
+    fn error(&mut self, message: &str, details: &Details);
+    fn warning(&mut self, message: &str, details: &Details);
+
     fn error_count(&self) -> usize;
+    fn warning_count(&self) -> usize;
 }
 
 pub struct ErrorReporter<T: Write> {
     io: T,
     need_newline_padding: bool,
     error_count: usize,
+    warning_count: usize,
 }
 
 impl<T: Write> ErrorReporter<T> {
@@ -19,19 +25,19 @@ impl<T: Write> ErrorReporter<T> {
             io: io,
             need_newline_padding: false,
             error_count: 0,
+            warning_count: 0,
         }
     }
-}
 
-impl<T: Write> ErrorSink for ErrorReporter<T> {
-    fn error(&mut self, message: &str, details: &[(&str, &SourceFile, &Loc)]) {
-        self.error_count += 1;
-
+    fn emit(&mut self, diagnostic_name: &str, diagnostic_color: usize, message: &str, details: &Details) {
         if self.need_newline_padding {
-            write!(self.io, "\n\n").unwrap();
+            write!(self.io, "\n").unwrap();
         }
 
-        write!(self.io, "\x1b[31;1merror:\x1b[0;1m {}\x1b[0m\n\n", message).unwrap();
+        write!(self.io, "\x1b[3{diagnostic_color};1m{diagnostic_name}:\x1b[0;1m {message}\x1b[0m\n\n",
+            diagnostic_name = diagnostic_name,
+            diagnostic_color = diagnostic_color,
+            message = message).unwrap();
 
         for &(ref message, ref source, ref loc) in details {
             let begin = source.line_for_pos(loc.begin_pos);
@@ -42,13 +48,16 @@ impl<T: Write> ErrorSink for ErrorReporter<T> {
                 let line_info = begin;
 
                 write!(self.io, "        \x1b[34;1m@ {}\x1b[0m\n", source.filename()).unwrap();
-                write!(self.io, "\x1b[34;1m{number:>7} |\x1b[0m {line_prefix}\x1b[31;1m{highlight}\x1b[0m{line_suffix}\n",
+
+                write!(self.io, "\x1b[34;1m{number:>7} |\x1b[0m {line_prefix}\x1b[3{diagnostic_color};1m{highlight}\x1b[0m{line_suffix}\n",
+                    diagnostic_color = diagnostic_color,
                     number = line_info.number,
                     line_prefix = &source.source()[line_info.begin_pos..loc.begin_pos],
                     highlight = &source.source()[loc.begin_pos..loc.end_pos],
                     line_suffix = &source.source()[loc.end_pos..line_info.end_pos].trim_right()).unwrap();
 
-                write!(self.io, "{pad:pad_len$}\x1b[31;1m{marker}\x1b[0;1m {message}\x1b[0m\n",
+                write!(self.io, "{pad:pad_len$}\x1b[3{diagnostic_color};1m{marker}\x1b[0;1m {message}\x1b[0m\n",
+                    diagnostic_color = diagnostic_color,
                     pad = "", pad_len = 10 + loc.begin_pos - line_info.begin_pos,
                     marker = "^".repeat(loc.end_pos - loc.begin_pos),
                     message = message).unwrap();
@@ -59,8 +68,24 @@ impl<T: Write> ErrorSink for ErrorReporter<T> {
 
         self.need_newline_padding = true;
     }
+}
+
+impl<T: Write> ErrorSink for ErrorReporter<T> {
+    fn error(&mut self, message: &str, details: &Details) {
+        self.error_count += 1;
+        self.emit("error", 1, message, details);
+    }
+
+    fn warning(&mut self, message: &str, details: &Details) {
+        self.error_count += 1;
+        self.emit("warning", 3, message, details);
+    }
 
     fn error_count(&self) -> usize {
         self.error_count
+    }
+
+    fn warning_count(&self) -> usize {
+        self.warning_count
     }
 }
