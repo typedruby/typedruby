22cb8e124366271b7662b39b7ee4b7ec29b80e1e
Charlie Somerville <charlie@charlie.bz>
2017-04-27 16:50:04 +1000

    fix directionality of lifetime dependency

--- src/typecheck/mod.rs
+++ src/typecheck/mod.rs
@@ -7,7 +7,7 @@ use object::MethodEntry;
 use typed_arena::Arena;
 use self::types::Types;
 
-pub fn check<'object, 'env: 'object>(env: &'env Environment<'object>, method: Rc<MethodEntry<'object>>) {
+pub fn check<'env, 'object: 'env>(env: &'env Environment<'object>, method: Rc<MethodEntry<'object>>) {
     let arena = Arena::new();
     let types = Types::new(&arena, &env.object);
 }
