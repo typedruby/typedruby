33935da9e2a1573aa944f08396fdb2fa73fe9592
Hailey Somerville <hailey@hailey.lol>
2017-05-01 17:01:52 +1000

    start to implement model for flow sensitive typing

---
+++ src/typecheck/flow.rs
@@ -0,0 +1,57 @@
+use std::rc::Rc;
+use typecheck::types::Type;
+
+#[derive(Debug)]
+pub enum Locals<'ty, 'object: 'ty> {
+    None,
+    Var {
+        parent: Rc<Locals<'ty, 'object>>,
+        name: String,
+        ty: &'ty Type<'ty, 'object>,
+    },
+}
+
+impl<'ty, 'object> Locals<'ty, 'object> {
+    pub fn assign(locals: Rc<Locals<'ty, 'object>>, name: String, ty: &'ty Type<'ty, 'object>) -> Rc<Locals<'ty, 'object>> {
+        Rc::new(Locals::Var {
+            parent: locals,
+            name: name,
+            ty: ty,
+        })
+    }
+}
+
+#[derive(Debug)]
+pub enum Computation<'ty, 'object: 'ty> {
+    Result(&'ty Type<'ty, 'object>, Rc<Locals<'ty, 'object>>),
+    Return(&'ty Type<'ty, 'object>, Rc<Locals<'ty, 'object>>),
+    Divergent(Rc<Computation<'ty, 'object>>, Rc<Computation<'ty, 'object>>),
+}
+
+impl<'ty, 'object> Computation<'ty, 'object> {
+    pub fn new_result(ty: &'ty Type<'ty, 'object>, locals: Rc<Locals<'ty, 'object>>) -> Rc<Computation<'ty, 'object>> {
+        Rc::new(Computation::Result(ty, locals))
+    }
+
+    pub fn seq<F>(comp: Rc<Computation<'ty, 'object>>, f: &F) -> Rc<Computation<'ty, 'object>>
+        where F: Fn(&'ty Type<'ty, 'object>, Rc<Locals<'ty, 'object>>) -> Rc<Computation<'ty, 'object>>
+    {
+        match *comp {
+            Computation::Result(ref ty, ref locals) => f(ty.clone(), locals.clone()),
+            Computation::Return(ref ty, ref locals) => { f(ty.clone(), locals.clone()); comp.clone() },
+            Computation::Divergent(ref a, ref b) => {
+                Rc::new(Computation::Divergent(
+                    Computation::seq(a.clone(), f),
+                    Computation::seq(b.clone(), f)))
+            },
+        }
+    }
+
+    pub fn converge(comp: Rc<Computation<'ty, 'object>>) -> Rc<Computation<'ty, 'object>> {
+        match *comp {
+            Computation::Result(..) => comp.clone(),
+            Computation::Return(..) => comp.clone(),
+            Computation::Divergent(ref a, ref b) => panic!("unimplemented"),
+        }
+    }
+}
