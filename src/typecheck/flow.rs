68ce920b818902324a56f50ccc52610e5c447bc8
Hailey Somerville <hailey@hailey.lol>
2017-05-02 14:51:41 +1000

    refactor Computation so that we can use instance method syntax

--- src/typecheck/flow.rs
+++ src/typecheck/flow.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use std::rc::Rc;
 use typecheck::types::Type;
 
@@ -22,48 +23,58 @@ impl<'ty, 'object> Locals<'ty, 'object> {
 }
 
 #[derive(Debug)]
-pub enum Computation<'ty, 'object: 'ty> {
+enum Computation_<'ty, 'object: 'ty> {
     Result(&'ty Type<'ty, 'object>, Rc<Locals<'ty, 'object>>),
     Return(&'ty Type<'ty, 'object>, Rc<Locals<'ty, 'object>>),
-    Divergent(Rc<Computation<'ty, 'object>>, Rc<Computation<'ty, 'object>>),
+    Divergent(Computation<'ty, 'object>, Computation<'ty, 'object>),
 }
 
-impl<'ty, 'object> Computation<'ty, 'object> {
-    pub fn new_result(ty: &'ty Type<'ty, 'object>, locals: Rc<Locals<'ty, 'object>>) -> Rc<Computation<'ty, 'object>> {
-        Rc::new(Computation::Result(ty, locals))
+#[derive(Clone)]
+pub struct Computation<'ty, 'object: 'ty>(Rc<Computation_<'ty, 'object>>);
+
+impl<'ty, 'object: 'ty> fmt::Debug for Computation<'ty, 'object> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl<'ty, 'object: 'ty> Computation<'ty, 'object> {
+    pub fn result(ty: &'ty Type<'ty, 'object>, locals: Rc<Locals<'ty, 'object>>) -> Computation<'ty, 'object> {
+        Computation(Rc::new(Computation_::Result(ty, locals)))
     }
 
-    pub fn seq<F>(comp: Rc<Computation<'ty, 'object>>, f: &F) -> Rc<Computation<'ty, 'object>>
-        where F: Fn(&'ty Type<'ty, 'object>, Rc<Locals<'ty, 'object>>) -> Rc<Computation<'ty, 'object>>
+    pub fn divergent(a: Computation<'ty, 'object>, b: Computation<'ty, 'object>) -> Computation<'ty, 'object> {
+        Computation(Rc::new(Computation_::Divergent(a, b)))
+    }
+
+    pub fn seq<F>(&self, f: &F) -> Computation<'ty, 'object>
+        where F: Fn(&'ty Type<'ty, 'object>, Rc<Locals<'ty, 'object>>) -> Computation<'ty, 'object>
     {
-        match *comp {
-            Computation::Result(ref ty, ref locals) => f(ty.clone(), locals.clone()),
-            Computation::Return(ref ty, ref locals) => { f(ty.clone(), locals.clone()); comp.clone() },
-            Computation::Divergent(ref a, ref b) =>
-                Rc::new(Computation::Divergent(
-                    Computation::seq(a.clone(), f),
-                    Computation::seq(b.clone(), f))),
+        match *self.0 {
+            Computation_::Result(ref ty, ref locals) => f(ty.clone(), locals.clone()),
+            Computation_::Return(ref ty, ref locals) => { f(ty.clone(), locals.clone()); self.clone() },
+            Computation_::Divergent(ref a, ref b) => Self::divergent(a.seq(f), b.seq(f)),
         }
     }
 
-    pub fn term<F>(comp: Rc<Computation<'ty, 'object>>, f: &F)
+    pub fn term<F>(&self, f: &F)
         where F: Fn(&'ty Type<'ty, 'object>, Rc<Locals<'ty, 'object>>)
     {
-        match *comp {
-            Computation::Result(ref ty, ref locals) |
-            Computation::Return(ref ty, ref locals) => f(ty.clone(), locals.clone()),
-            Computation::Divergent(ref a, ref b) => {
-                Computation::term(a.clone(), f);
-                Computation::term(b.clone(), f);
+        match *self.0 {
+            Computation_::Result(ref ty, ref locals) |
+            Computation_::Return(ref ty, ref locals) => f(ty.clone(), locals.clone()),
+            Computation_::Divergent(ref a, ref b) => {
+                a.term(f);
+                b.term(f);
             },
         }
     }
 
-    pub fn converge(comp: Rc<Computation<'ty, 'object>>) -> Rc<Computation<'ty, 'object>> {
-        match *comp {
-            Computation::Result(..) => comp.clone(),
-            Computation::Return(..) => comp.clone(),
-            Computation::Divergent(ref a, ref b) => panic!("unimplemented"),
+    pub fn converge(&self) -> Computation<'ty, 'object> {
+        match *self.0 {
+            Computation_::Result(..) => self.clone(),
+            Computation_::Return(..) => self.clone(),
+            Computation_::Divergent(ref a, ref b) => panic!("unimplemented"),
         }
     }
 }
