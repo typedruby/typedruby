124a15cbee5faaa7d91ade849a7b8505af68f743
Hailey Somerville <hailey@hailey.lol>
2017-05-29 13:47:32 +1000

    implement autopinning for locals

--- src/typecheck/flow.rs
+++ src/typecheck/flow.rs
@@ -66,6 +66,7 @@ impl<'ty, 'object> LocalEntry<'ty, 'object> {
 pub struct Locals_<'ty, 'object: 'ty> {
     parent: Option<Locals<'ty, 'object>>,
     vars: TreeMap<String, LocalEntry<'ty, 'object>>,
+    autopin: usize,
 }
 
 #[derive(Debug)]
@@ -110,23 +111,31 @@ impl<'ty, 'object> Locals<'ty, 'object> {
     }
 
     pub fn new() -> Locals<'ty, 'object> {
-        Self::new_(Locals_ { parent: None, vars: TreeMap::new() })
+        Self::new_(Locals_ { parent: None, vars: TreeMap::new(), autopin: 0 })
     }
 
     pub fn extend(&self) -> Locals<'ty, 'object> {
-        Self::new_(Locals_ { parent: Some(Locals(self.0.clone())), vars: TreeMap::new() })
+        Self::new_(Locals_ { parent: Some(Locals(self.0.clone())), vars: TreeMap::new(), autopin: 0 })
     }
 
     pub fn unextend(&self) -> Locals<'ty, 'object> {
         self.0.parent.as_ref().expect("unbalanced extend/unextend (parent is None)").clone()
     }
 
+    pub fn autopin(&self) -> Locals<'ty, 'object> {
+        Self::new_(Locals_ { parent: self.0.parent.clone(), vars: self.0.vars.clone(), autopin: self.0.autopin + 1 })
+    }
+
+    pub fn unautopin(&self) -> Locals<'ty, 'object> {
+        Self::new_(Locals_ { parent: self.0.parent.clone(), vars: self.0.vars.clone(), autopin: self.0.autopin - 1 })
+    }
+
     fn update_parent(&self, parent: Option<Locals<'ty, 'object>>) -> Locals<'ty, 'object> {
-        Self::new_(Locals_ { parent: parent, vars: self.0.vars.clone() })
+        Self::new_(Locals_ { parent: parent, vars: self.0.vars.clone(), autopin: self.0.autopin })
     }
 
     fn update_vars(&self, vars: TreeMap<String, LocalEntry<'ty, 'object>>) -> Locals<'ty, 'object> {
-        Self::new_(Locals_ { parent: self.0.parent.clone(), vars: vars })
+        Self::new_(Locals_ { parent: self.0.parent.clone(), vars: vars, autopin: self.0.autopin })
     }
 
     fn get_var_direct(&self, name: &str) -> LocalEntry<'ty, 'object> {
@@ -183,7 +192,8 @@ impl<'ty, 'object> Locals<'ty, 'object> {
     pub fn assign(&self, name: String, ty: &'ty Type<'ty, 'object>) -> (Option<&'ty Type<'ty, 'object>>, Locals<'ty, 'object>) {
         if let Some(local) = self.0.vars.get(&name) {
             return match *local {
-                LocalEntry::Bound(_) => (None, self.insert_var(name, LocalEntry::Bound(ty))),
+                LocalEntry::Bound(_) if self.0.autopin == 0 => (None, self.insert_var(name, LocalEntry::Bound(ty))),
+                LocalEntry::Bound(ty) => (Some(ty), self.insert_var(name, LocalEntry::Pinned(ty))),
                 LocalEntry::Pinned(ty) => (Some(ty), self.clone()),
                 LocalEntry::ConditionallyPinned(ty) => (Some(ty), self.clone()),
                 LocalEntry::Unbound => panic!("should not happen"),
@@ -218,6 +228,8 @@ impl<'ty, 'object> Locals<'ty, 'object> {
     }
 
     pub fn merge<'env>(&self, other: Locals<'ty, 'object>, tyenv: &TypeEnv<'ty, 'env, 'object>, merges: &mut Vec<LocalEntryMerge<'ty, 'object>>) -> Locals<'ty, 'object> {
+        assert!(self.0.autopin == other.0.autopin);
+
         let mut names = HashSet::new();
         names.extend(self.0.vars.keys());
         names.extend(other.0.vars.keys());
