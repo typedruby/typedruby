d81e596933a1e738da01c82fab2d735e372db7a1
Hailey Somerville <hailey@hailey.lol>
2017-06-01 17:23:51 +1000

    extract locals into its own mod

--- src/typecheck/flow.rs
+++ src/typecheck/flow.rs
@@ -1,73 +1,9 @@
 use std::fmt;
 use std::rc::Rc;
 use typecheck::types::{TypeEnv, Type};
+use typecheck::locals::{Locals, LocalEntryMerge};
 use ast::Loc;
-use immutable_map::TreeMap;
 use util::Or;
-use std::collections::HashSet;
-
-#[derive(Debug,Clone)]
-pub enum LocalEntry<'ty, 'object: 'ty> {
-    Unbound,
-    Bound(&'ty Type<'ty, 'object>),
-    Pinned(&'ty Type<'ty, 'object>),
-    ConditionallyPinned(&'ty Type<'ty, 'object>),
-}
-
-#[derive(Debug,Clone)]
-pub enum LocalEntryMerge<'ty, 'object: 'ty> {
-    Ok(LocalEntry<'ty, 'object>),
-    MustMatch(LocalEntry<'ty, 'object>, &'ty Type<'ty, 'object>, &'ty Type<'ty, 'object>)
-}
-
-impl<'ty, 'object> LocalEntry<'ty, 'object> {
-    pub fn merge<'env>(self, other: LocalEntry<'ty, 'object>, tyenv: &TypeEnv<'ty, 'env, 'object>) -> LocalEntryMerge<'ty, 'object> {
-        match (self, other) {
-            (LocalEntry::Unbound, LocalEntry::Unbound) =>
-                LocalEntryMerge::Ok(LocalEntry::Unbound),
-            (LocalEntry::Unbound, LocalEntry::Bound(ty)) =>
-                LocalEntryMerge::Ok(LocalEntry::Bound(tyenv.nillable(ty.loc(), ty))),
-            (LocalEntry::Unbound, LocalEntry::Pinned(ty)) =>
-                LocalEntryMerge::Ok(LocalEntry::ConditionallyPinned(ty)),
-            (LocalEntry::Unbound, LocalEntry::ConditionallyPinned(ty)) =>
-                LocalEntryMerge::Ok(LocalEntry::ConditionallyPinned(ty)),
-
-            (LocalEntry::Bound(ty), LocalEntry::Unbound) =>
-                LocalEntryMerge::Ok(LocalEntry::Bound(tyenv.nillable(ty.loc(), ty))),
-            (LocalEntry::Bound(tya), LocalEntry::Bound(tyb)) =>
-                LocalEntryMerge::Ok(LocalEntry::Bound(tyenv.union(tya.loc() /* TODO incorporate tyb too */, tya, tyb))),
-            (LocalEntry::Bound(bound_ty), LocalEntry::Pinned(pinned_ty)) =>
-                LocalEntryMerge::MustMatch(LocalEntry::Pinned(pinned_ty), pinned_ty, bound_ty),
-            (LocalEntry::Bound(bound_ty), LocalEntry::ConditionallyPinned(pinned_ty)) =>
-                LocalEntryMerge::MustMatch(LocalEntry::ConditionallyPinned(pinned_ty), pinned_ty, bound_ty),
-
-            (LocalEntry::Pinned(pinned_ty), LocalEntry::Unbound) =>
-                LocalEntryMerge::Ok(LocalEntry::ConditionallyPinned(pinned_ty)),
-            (LocalEntry::Pinned(pinned_ty), LocalEntry::Bound(bound_ty)) =>
-                LocalEntryMerge::MustMatch(LocalEntry::Pinned(pinned_ty), pinned_ty, bound_ty),
-            (LocalEntry::Pinned(tya), LocalEntry::Pinned(tyb)) =>
-                LocalEntryMerge::MustMatch(LocalEntry::Pinned(tya), tya, tyb),
-            (LocalEntry::Pinned(tya), LocalEntry::ConditionallyPinned(tyb)) =>
-                LocalEntryMerge::MustMatch(LocalEntry::ConditionallyPinned(tyb), tyb, tya),
-
-            (LocalEntry::ConditionallyPinned(pinned_ty), LocalEntry::Unbound) =>
-                LocalEntryMerge::Ok(LocalEntry::ConditionallyPinned(pinned_ty)),
-            (LocalEntry::ConditionallyPinned(pinned_ty), LocalEntry::Bound(bound_ty)) =>
-                LocalEntryMerge::MustMatch(LocalEntry::ConditionallyPinned(pinned_ty), pinned_ty, bound_ty),
-            (LocalEntry::ConditionallyPinned(tya), LocalEntry::Pinned(tyb)) =>
-                LocalEntryMerge::MustMatch(LocalEntry::ConditionallyPinned(tya), tya, tyb),
-            (LocalEntry::ConditionallyPinned(tya), LocalEntry::ConditionallyPinned(tyb)) =>
-                LocalEntryMerge::MustMatch(LocalEntry::ConditionallyPinned(tya), tya, tyb),
-        }
-    }
-}
-
-#[derive(Debug)]
-pub struct Locals_<'ty, 'object: 'ty> {
-    parent: Option<Locals<'ty, 'object>>,
-    vars: TreeMap<String, LocalEntry<'ty, 'object>>,
-    autopin: usize,
-}
 
 #[derive(Debug)]
 pub struct ComputationPredicate<'ty, 'object: 'ty> {
@@ -99,160 +35,6 @@ impl<'ty, 'object> ComputationPredicate<'ty, 'object> {
             falsy: Computation::divergent_option(self.falsy, other.falsy),
             non_result: Computation::divergent_option(self.non_result, other.non_result),
         }
-    }
-}
-
-#[derive(Debug,Clone)]
-pub struct Locals<'ty, 'object: 'ty>(Rc<Locals_<'ty, 'object>>);
-
-impl<'ty, 'object> Locals<'ty, 'object> {
-    fn new_(l: Locals_<'ty, 'object>) -> Locals<'ty, 'object>{
-        Locals(Rc::new(l))
-    }
-
-    pub fn new() -> Locals<'ty, 'object> {
-        Self::new_(Locals_ { parent: None, vars: TreeMap::new(), autopin: 0 })
-    }
-
-    pub fn extend(&self) -> Locals<'ty, 'object> {
-        Self::new_(Locals_ { parent: Some(Locals(self.0.clone())), vars: TreeMap::new(), autopin: 0 })
-    }
-
-    pub fn unextend(&self) -> Locals<'ty, 'object> {
-        self.0.parent.as_ref().expect("unbalanced extend/unextend (parent is None)").clone()
-    }
-
-    pub fn autopin(&self) -> Locals<'ty, 'object> {
-        Self::new_(Locals_ { parent: self.0.parent.clone(), vars: self.0.vars.clone(), autopin: self.0.autopin + 1 })
-    }
-
-    pub fn unautopin(&self) -> Locals<'ty, 'object> {
-        Self::new_(Locals_ { parent: self.0.parent.clone(), vars: self.0.vars.clone(), autopin: self.0.autopin - 1 })
-    }
-
-    fn update_parent(&self, parent: Option<Locals<'ty, 'object>>) -> Locals<'ty, 'object> {
-        Self::new_(Locals_ { parent: parent, vars: self.0.vars.clone(), autopin: self.0.autopin })
-    }
-
-    fn update_vars(&self, vars: TreeMap<String, LocalEntry<'ty, 'object>>) -> Locals<'ty, 'object> {
-        Self::new_(Locals_ { parent: self.0.parent.clone(), vars: vars, autopin: self.0.autopin })
-    }
-
-    fn get_var_direct(&self, name: &str) -> LocalEntry<'ty, 'object> {
-        match self.0.vars.get(name) {
-            Some(entry) => entry.clone(),
-            None => LocalEntry::Unbound,
-        }
-    }
-
-    fn insert_var(&self, name: String, entry: LocalEntry<'ty, 'object>) -> Locals<'ty, 'object> {
-        self.update_vars(self.0.vars.insert(name, entry))
-    }
-
-    fn update_upvar<F>(&self, name: &str, f: &F) -> (LocalEntry<'ty, 'object>, Option<Locals<'ty, 'object>>)
-        where F: Fn(&LocalEntry<'ty, 'object>) -> (LocalEntry<'ty, 'object>)
-    {
-        if let Some(local) = self.0.vars.get(name) {
-            let new_local = f(local);
-
-            (new_local.clone(), Some(self.insert_var(name.to_owned(), new_local)))
-        } else if let Some(ref parent) = self.0.parent {
-            let (x, parent) = parent.update_upvar(name, f);
-
-            (x, parent.map(|parent| self.update_parent(Some(parent))))
-        } else {
-            (LocalEntry::Unbound, None)
-        }
-    }
-
-    pub fn lookup(&self, name: &str) -> (LocalEntry<'ty, 'object>, Locals<'ty, 'object>) {
-        if let Some(local) = self.0.vars.get(name) {
-            (local.clone(), self.clone())
-        } else {
-            let updated = self.update_upvar(name, &|local|
-                match *local {
-                    LocalEntry::Unbound => LocalEntry::Unbound,
-                    LocalEntry::Bound(ty) => LocalEntry::Pinned(ty),
-                    LocalEntry::Pinned(ty) => LocalEntry::Pinned(ty),
-                    LocalEntry::ConditionallyPinned(ty) => LocalEntry::ConditionallyPinned(ty),
-                }
-            );
-
-            match updated {
-                (x, Some(locals)) => (x, locals),
-                (x, None) => (x, self.clone()),
-            }
-        }
-    }
-
-    pub fn assign_shadow(&self, name: String, ty: &'ty Type<'ty, 'object>) -> Locals<'ty, 'object> {
-        self.insert_var(name, LocalEntry::Bound(ty))
-    }
-
-    pub fn assign(&self, name: String, ty: &'ty Type<'ty, 'object>) -> (Option<&'ty Type<'ty, 'object>>, Locals<'ty, 'object>) {
-        if let Some(local) = self.0.vars.get(&name) {
-            return match *local {
-                LocalEntry::Bound(_) if self.0.autopin == 0 => (None, self.insert_var(name, LocalEntry::Bound(ty))),
-                LocalEntry::Bound(ty) => (Some(ty), self.insert_var(name, LocalEntry::Pinned(ty))),
-                LocalEntry::Pinned(ty) => (Some(ty), self.clone()),
-                LocalEntry::ConditionallyPinned(ty) => (Some(ty), self.clone()),
-                LocalEntry::Unbound => panic!("should not happen"),
-            }
-        }
-
-        if let Some(ref parent) = self.0.parent {
-            let (entry, locals) = parent.update_upvar(&name, &|local| {
-                match *local {
-                    LocalEntry::Bound(_) => LocalEntry::Pinned(ty),
-                    LocalEntry::Pinned(ty) => LocalEntry::Pinned(ty),
-                    LocalEntry::ConditionallyPinned(ty) => LocalEntry::Pinned(ty),
-                    LocalEntry::Unbound => panic!("should not happen"),
-                }
-            });
-
-            if let LocalEntry::Pinned(pinned_ty) = entry {
-                return (Some(pinned_ty), locals.unwrap_or_else(|| self.clone()))
-            }
-        }
-
-        (None, self.insert_var(name, LocalEntry::Bound(ty)))
-    }
-
-    pub fn refine(&self, name: String, ty: &'ty Type<'ty, 'object>) -> Locals<'ty, 'object> {
-        match self.get_var_direct(&name) {
-            LocalEntry::Unbound => panic!("should not happen"),
-            LocalEntry::Bound(_) => self.insert_var(name, LocalEntry::Bound(ty)),
-            LocalEntry::Pinned(_) => self.clone(),
-            LocalEntry::ConditionallyPinned(_) => self.clone(),
-        }
-    }
-
-    pub fn merge<'env>(&self, other: Locals<'ty, 'object>, tyenv: &TypeEnv<'ty, 'env, 'object>, merges: &mut Vec<LocalEntryMerge<'ty, 'object>>) -> Locals<'ty, 'object> {
-        assert!(self.0.autopin == other.0.autopin);
-
-        let mut names = HashSet::new();
-        names.extend(self.0.vars.keys());
-        names.extend(other.0.vars.keys());
-
-        let vars = names.into_iter().fold(TreeMap::new(), |map, name| {
-            let merge = self.get_var_direct(name).merge(other.get_var_direct(name), tyenv);
-
-            merges.push(merge.clone());
-
-            match merge {
-                LocalEntryMerge::Ok(entry) |
-                LocalEntryMerge::MustMatch(entry, ..) =>
-                    map.insert(name.clone(), entry)
-            }
-        });
-
-        self.update_vars(vars)
-    }
-}
-
-impl<'ty, 'object> PartialEq for Locals<'ty, 'object> {
-    fn eq(&self, other: &Locals<'ty, 'object>) -> bool {
-        (&*self.0 as *const _) == (&*other.0 as *const _)
     }
 }
 
@@ -399,26 +181,26 @@ impl<'ty, 'object: 'ty> Computation<'ty, 'object> {
     }
 
     pub fn extract_results<'env>(&self, loc: &Loc, tyenv: &TypeEnv<'ty, 'env, 'object>, merges: &mut Vec<LocalEntryMerge<'ty, 'object>>)
-        -> Or<(&'ty Type<'ty, 'object>, Locals<'ty, 'object>), Computation<'ty, 'object>>
+        -> EvalResult<'ty, 'object, &'ty Type<'ty, 'object>>
     {
         let converged = self.converge_results(loc, tyenv, merges);
 
         match *converged.0 {
-            Computation_::Result(ty, ref locals) => Or::Left((ty, locals.clone())),
+            Computation_::Result(ty, ref locals) => EvalResult::Ok(ty, locals.clone()),
 
             Computation_::Raise(..) |
             Computation_::Return(..) |
             Computation_::Redo |
-            Computation_::Retry => Or::Right(converged.clone()),
+            Computation_::Retry => EvalResult::NonResult(converged.clone()),
 
             Computation_::Divergent(ref a, ref b) => {
                 // if there were any result computations, converge_results
                 // guarantees that they will have been collapsed into the
                 // left hand side of the divergent computation it returns.
                 if let Computation_::Result(ty, ref locals) = *a.0 {
-                    Or::Both((ty, locals.clone()), b.clone())
+                    EvalResult::Both(ty, locals.clone(), b.clone())
                 } else {
-                    Or::Right(converged.clone())
+                    EvalResult::NonResult(converged.clone())
                 }
             }
         }
---
+++ src/typecheck/locals.rs
@@ -0,0 +1,222 @@
+use std::rc::Rc;
+use std::collections::HashSet;
+use immutable_map::TreeMap;
+
+use typecheck::types::{TypeEnv, Type};
+
+#[derive(Debug,Clone)]
+pub enum LocalEntry<'ty, 'object: 'ty> {
+    Unbound,
+    Bound(&'ty Type<'ty, 'object>),
+    Pinned(&'ty Type<'ty, 'object>),
+    ConditionallyPinned(&'ty Type<'ty, 'object>),
+}
+
+#[derive(Debug,Clone)]
+pub enum LocalEntryMerge<'ty, 'object: 'ty> {
+    Ok(LocalEntry<'ty, 'object>),
+    MustMatch(LocalEntry<'ty, 'object>, &'ty Type<'ty, 'object>, &'ty Type<'ty, 'object>)
+}
+
+impl<'ty, 'object> LocalEntry<'ty, 'object> {
+    pub fn merge<'env>(self, other: LocalEntry<'ty, 'object>, tyenv: &TypeEnv<'ty, 'env, 'object>) -> LocalEntryMerge<'ty, 'object> {
+        match (self, other) {
+            (LocalEntry::Unbound, LocalEntry::Unbound) =>
+                LocalEntryMerge::Ok(LocalEntry::Unbound),
+            (LocalEntry::Unbound, LocalEntry::Bound(ty)) =>
+                LocalEntryMerge::Ok(LocalEntry::Bound(tyenv.nillable(ty.loc(), ty))),
+            (LocalEntry::Unbound, LocalEntry::Pinned(ty)) =>
+                LocalEntryMerge::Ok(LocalEntry::ConditionallyPinned(ty)),
+            (LocalEntry::Unbound, LocalEntry::ConditionallyPinned(ty)) =>
+                LocalEntryMerge::Ok(LocalEntry::ConditionallyPinned(ty)),
+
+            (LocalEntry::Bound(ty), LocalEntry::Unbound) =>
+                LocalEntryMerge::Ok(LocalEntry::Bound(tyenv.nillable(ty.loc(), ty))),
+            (LocalEntry::Bound(tya), LocalEntry::Bound(tyb)) =>
+                LocalEntryMerge::Ok(LocalEntry::Bound(tyenv.union(tya.loc() /* TODO incorporate tyb too */, tya, tyb))),
+            (LocalEntry::Bound(bound_ty), LocalEntry::Pinned(pinned_ty)) =>
+                LocalEntryMerge::MustMatch(LocalEntry::Pinned(pinned_ty), pinned_ty, bound_ty),
+            (LocalEntry::Bound(bound_ty), LocalEntry::ConditionallyPinned(pinned_ty)) =>
+                LocalEntryMerge::MustMatch(LocalEntry::ConditionallyPinned(pinned_ty), pinned_ty, bound_ty),
+
+            (LocalEntry::Pinned(pinned_ty), LocalEntry::Unbound) =>
+                LocalEntryMerge::Ok(LocalEntry::ConditionallyPinned(pinned_ty)),
+            (LocalEntry::Pinned(pinned_ty), LocalEntry::Bound(bound_ty)) =>
+                LocalEntryMerge::MustMatch(LocalEntry::Pinned(pinned_ty), pinned_ty, bound_ty),
+            (LocalEntry::Pinned(tya), LocalEntry::Pinned(tyb)) =>
+                LocalEntryMerge::MustMatch(LocalEntry::Pinned(tya), tya, tyb),
+            (LocalEntry::Pinned(tya), LocalEntry::ConditionallyPinned(tyb)) =>
+                LocalEntryMerge::MustMatch(LocalEntry::ConditionallyPinned(tyb), tyb, tya),
+
+            (LocalEntry::ConditionallyPinned(pinned_ty), LocalEntry::Unbound) =>
+                LocalEntryMerge::Ok(LocalEntry::ConditionallyPinned(pinned_ty)),
+            (LocalEntry::ConditionallyPinned(pinned_ty), LocalEntry::Bound(bound_ty)) =>
+                LocalEntryMerge::MustMatch(LocalEntry::ConditionallyPinned(pinned_ty), pinned_ty, bound_ty),
+            (LocalEntry::ConditionallyPinned(tya), LocalEntry::Pinned(tyb)) =>
+                LocalEntryMerge::MustMatch(LocalEntry::ConditionallyPinned(tya), tya, tyb),
+            (LocalEntry::ConditionallyPinned(tya), LocalEntry::ConditionallyPinned(tyb)) =>
+                LocalEntryMerge::MustMatch(LocalEntry::ConditionallyPinned(tya), tya, tyb),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Locals_<'ty, 'object: 'ty> {
+    parent: Option<Locals<'ty, 'object>>,
+    vars: TreeMap<String, LocalEntry<'ty, 'object>>,
+    autopin: usize,
+}
+
+#[derive(Debug,Clone)]
+pub struct Locals<'ty, 'object: 'ty>(Rc<Locals_<'ty, 'object>>);
+
+impl<'ty, 'object> Locals<'ty, 'object> {
+    fn new_(l: Locals_<'ty, 'object>) -> Locals<'ty, 'object>{
+        Locals(Rc::new(l))
+    }
+
+    pub fn new() -> Locals<'ty, 'object> {
+        Self::new_(Locals_ { parent: None, vars: TreeMap::new(), autopin: 0 })
+    }
+
+    pub fn extend(&self) -> Locals<'ty, 'object> {
+        Self::new_(Locals_ { parent: Some(Locals(self.0.clone())), vars: TreeMap::new(), autopin: 0 })
+    }
+
+    pub fn unextend(&self) -> Locals<'ty, 'object> {
+        self.0.parent.as_ref().expect("unbalanced extend/unextend (parent is None)").clone()
+    }
+
+    pub fn autopin(&self) -> Locals<'ty, 'object> {
+        Self::new_(Locals_ { parent: self.0.parent.clone(), vars: self.0.vars.clone(), autopin: self.0.autopin + 1 })
+    }
+
+    pub fn unautopin(&self) -> Locals<'ty, 'object> {
+        Self::new_(Locals_ { parent: self.0.parent.clone(), vars: self.0.vars.clone(), autopin: self.0.autopin - 1 })
+    }
+
+    fn update_parent(&self, parent: Option<Locals<'ty, 'object>>) -> Locals<'ty, 'object> {
+        Self::new_(Locals_ { parent: parent, vars: self.0.vars.clone(), autopin: self.0.autopin })
+    }
+
+    fn update_vars(&self, vars: TreeMap<String, LocalEntry<'ty, 'object>>) -> Locals<'ty, 'object> {
+        Self::new_(Locals_ { parent: self.0.parent.clone(), vars: vars, autopin: self.0.autopin })
+    }
+
+    fn get_var_direct(&self, name: &str) -> LocalEntry<'ty, 'object> {
+        match self.0.vars.get(name) {
+            Some(entry) => entry.clone(),
+            None => LocalEntry::Unbound,
+        }
+    }
+
+    fn insert_var(&self, name: String, entry: LocalEntry<'ty, 'object>) -> Locals<'ty, 'object> {
+        self.update_vars(self.0.vars.insert(name, entry))
+    }
+
+    fn update_upvar<F>(&self, name: &str, f: &F) -> (LocalEntry<'ty, 'object>, Option<Locals<'ty, 'object>>)
+        where F: Fn(&LocalEntry<'ty, 'object>) -> (LocalEntry<'ty, 'object>)
+    {
+        if let Some(local) = self.0.vars.get(name) {
+            let new_local = f(local);
+
+            (new_local.clone(), Some(self.insert_var(name.to_owned(), new_local)))
+        } else if let Some(ref parent) = self.0.parent {
+            let (x, parent) = parent.update_upvar(name, f);
+
+            (x, parent.map(|parent| self.update_parent(Some(parent))))
+        } else {
+            (LocalEntry::Unbound, None)
+        }
+    }
+
+    pub fn lookup(&self, name: &str) -> (LocalEntry<'ty, 'object>, Locals<'ty, 'object>) {
+        if let Some(local) = self.0.vars.get(name) {
+            (local.clone(), self.clone())
+        } else {
+            let updated = self.update_upvar(name, &|local|
+                match *local {
+                    LocalEntry::Unbound => LocalEntry::Unbound,
+                    LocalEntry::Bound(ty) => LocalEntry::Pinned(ty),
+                    LocalEntry::Pinned(ty) => LocalEntry::Pinned(ty),
+                    LocalEntry::ConditionallyPinned(ty) => LocalEntry::ConditionallyPinned(ty),
+                }
+            );
+
+            match updated {
+                (x, Some(locals)) => (x, locals),
+                (x, None) => (x, self.clone()),
+            }
+        }
+    }
+
+    pub fn assign_shadow(&self, name: String, ty: &'ty Type<'ty, 'object>) -> Locals<'ty, 'object> {
+        self.insert_var(name, LocalEntry::Bound(ty))
+    }
+
+    pub fn assign(&self, name: String, ty: &'ty Type<'ty, 'object>) -> (Option<&'ty Type<'ty, 'object>>, Locals<'ty, 'object>) {
+        if let Some(local) = self.0.vars.get(&name) {
+            return match *local {
+                LocalEntry::Bound(_) if self.0.autopin == 0 => (None, self.insert_var(name, LocalEntry::Bound(ty))),
+                LocalEntry::Bound(ty) => (Some(ty), self.insert_var(name, LocalEntry::Pinned(ty))),
+                LocalEntry::Pinned(ty) => (Some(ty), self.clone()),
+                LocalEntry::ConditionallyPinned(ty) => (Some(ty), self.clone()),
+                LocalEntry::Unbound => panic!("should not happen"),
+            }
+        }
+
+        if let Some(ref parent) = self.0.parent {
+            let (entry, locals) = parent.update_upvar(&name, &|local| {
+                match *local {
+                    LocalEntry::Bound(_) => LocalEntry::Pinned(ty),
+                    LocalEntry::Pinned(ty) => LocalEntry::Pinned(ty),
+                    LocalEntry::ConditionallyPinned(ty) => LocalEntry::Pinned(ty),
+                    LocalEntry::Unbound => panic!("should not happen"),
+                }
+            });
+
+            if let LocalEntry::Pinned(pinned_ty) = entry {
+                return (Some(pinned_ty), locals.unwrap_or_else(|| self.clone()))
+            }
+        }
+
+        (None, self.insert_var(name, LocalEntry::Bound(ty)))
+    }
+
+    pub fn refine(&self, name: String, ty: &'ty Type<'ty, 'object>) -> Locals<'ty, 'object> {
+        match self.get_var_direct(&name) {
+            LocalEntry::Unbound => panic!("should not happen"),
+            LocalEntry::Bound(_) => self.insert_var(name, LocalEntry::Bound(ty)),
+            LocalEntry::Pinned(_) => self.clone(),
+            LocalEntry::ConditionallyPinned(_) => self.clone(),
+        }
+    }
+
+    pub fn merge<'env>(&self, other: Locals<'ty, 'object>, tyenv: &TypeEnv<'ty, 'env, 'object>, merges: &mut Vec<LocalEntryMerge<'ty, 'object>>) -> Locals<'ty, 'object> {
+        assert!(self.0.autopin == other.0.autopin);
+
+        let mut names = HashSet::new();
+        names.extend(self.0.vars.keys());
+        names.extend(other.0.vars.keys());
+
+        let vars = names.into_iter().fold(TreeMap::new(), |map, name| {
+            let merge = self.get_var_direct(name).merge(other.get_var_direct(name), tyenv);
+
+            merges.push(merge.clone());
+
+            match merge {
+                LocalEntryMerge::Ok(entry) |
+                LocalEntryMerge::MustMatch(entry, ..) =>
+                    map.insert(name.clone(), entry)
+            }
+        });
+
+        self.update_vars(vars)
+    }
+}
+
+impl<'ty, 'object> PartialEq for Locals<'ty, 'object> {
+    fn eq(&self, other: &Locals<'ty, 'object>) -> bool {
+        (&*self.0 as *const _) == (&*other.0 as *const _)
+    }
+}
