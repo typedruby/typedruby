76b489ef51b0a5ac7442e33f4d5c005643671180
Vicent Marti <tanoku@gmail.com>
2017-06-05 19:00:03 +0200

    whitequark: Fix masgn in condition for 2.4

--- tests/whitequark.rs
+++ tests/whitequark.rs
@@ -1311,14 +1311,17 @@ fn test_if_elsif() {
 	parse_and_cmp!(code, sexp, OPTIONS);
 }
 
-// TODO: Known breakage
 #[test]
-#[should_panic]
 fn test_if_masgn_24() {
 	let code = "if (a, b = foo); end";
 	let sexp = r##"
 (if
-  (begin nil) nil nil)
+  (begin
+    (masgn
+      (mlhs
+        (lvasgn :a)
+        (lvasgn :b))
+      (lvar :foo))) nil nil)
 "##;
 	parse_and_cmp!(code, sexp, OPTIONS);
 }
