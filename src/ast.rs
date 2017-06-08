01559ede6764daeb7b37bf43ac2f1bd242328d43
Hailey Somerville <hailey@hailey.lol>
2017-06-08 15:53:39 +1000

    update ruby_parser

--- src/ast.rs
+++ src/ast.rs
@@ -1,5 +1,3 @@
 extern crate ruby_parser;
 
-pub use self::ruby_parser::{parse, SourceFile, Id, Node, Loc, Diagnostic};
-pub use self::ruby_parser::Error as ParserError;
-pub use self::ruby_parser::Level as DiagnosticLevel;
+pub use self::ruby_parser::{parse, SourceFile, Id, Node, Loc, Diagnostic, Level};
