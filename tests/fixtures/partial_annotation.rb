bce6e0d9f1f344af412eb4b173bda956ea2044fd
Charlie Somerville <charlie@charlie.bz>
2017-09-12 16:49:39 +0100

    add annotation status tests

---
+++ tests/fixtures/partial_annotation.rb
@@ -0,0 +1,20 @@
+def f1
+end
+
+def f2(Integer x)
+end
+
+def f3(Integer x, y, Integer z)
+end
+
+def f4(x) => Integer
+end
+
+def f5(x, y, Integer z) => Integer
+end
+
+def f6 => Integer
+end
+
+def f7(Integer x, Integer y) => Integer
+end
