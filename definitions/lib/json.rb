b8dd7ad79594068426450631229836f535710d7b
Parker Moore <parkr@github.com>
2017-10-16 21:37:38 -0400

    These pass muster on our checks internally.

--- definitions/lib/json.rb
+++ definitions/lib/json.rb
@@ -1,10 +1,13 @@
 module JSON
   def self.dump(:any object) => String
   end
-  
-  def self.generate(:any object, :any options) => String
+
+  def self.generate(:any object, (nil | JSON::State | Hash::[Symbol, :any]) options) => String
   end
-  
+
   def self.parse(String json) => :any
+  end
+
+  class State
   end
 end
