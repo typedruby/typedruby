d47c22df68ce38e9ea7289b8f8fe835fc1ed740d
Parker Moore <parkrmoore@gmail.com>
2017-10-16 20:58:49 -0400

    Stub JSON.generate and JSON.parse

--- definitions/lib/json.rb
+++ definitions/lib/json.rb
@@ -1,4 +1,10 @@
 module JSON
   def self.dump(:any object) => String
   end
+  
+  def self.generate(:any object, :any options) => String
+  end
+  
+  def self.parse(:any objcet) => Hash::[String, T]
+  end
 end
