2de010a392b1f1138db6c0c56da9c22f3c506580
Charlie Somerville <charlie@charlie.bz>
2018-01-16 15:08:58 +1100

    move socket_path fn up to remote mod root

--- src/remote/mod.rs
+++ src/remote/mod.rs
@@ -1,3 +1,15 @@
+use std::env;
+use std::io;
+use std::path::PathBuf;
+
 pub mod client;
 pub mod protocol;
 pub mod server;
+
+pub fn socket_path() -> Result<PathBuf, io::Error> {
+    let mut path = env::current_dir()?;
+
+    path.push(".typedruby.sock");
+
+    Ok(path)
+}
